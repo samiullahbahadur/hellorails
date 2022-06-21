3801ff3840903062e9673843f92c4e732551d058
samiullahbahadur <bahadursamiullah@gmail.com>
2022-06-21 13:56:38 +0430

    Create pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
- # get 'home/index'
- root 'home#index'
+  #get 'pages/index'
+  root 'pages#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
